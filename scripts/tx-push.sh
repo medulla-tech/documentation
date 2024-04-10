5ebb2cff497b3fbe16e97cb29cbf06b7dd77f801
Nicolas Lécureuil <neoclust@mageia.org>
2024-04-10 11:59:11 +0200

    (i18n) Fix scripts
    Add script to upload translations on transifex
    Remove debugs on the pull script

--- scripts/tx-push.sh
+++ scripts/tx-push.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 # This script maps PO and POT files to transifex ressources
-# on https://www.transifex.com
-# tx script can be downloaded on https://developers.transifex.com/docs/cli
-#
+# on https://transifex.mandriva.com
+
 SCRIPT_PROJECT=`pwd`
 
 which tx > /dev/null
 if [ $? -ne 0 ]; then
-        echo "Install the transifex client v0.4 (pip install transifex-client==0.4)"
-        exit 1
+    echo "Install the transifex client v0.4 (pip install transifex-client==0.4)"
+    exit 1
 fi
 
-#TODO: Adapt to new transifex.
-#test -d .tx || tx init --host=https://www.transifex.com
+#test -d .tx || tx init --host=https://fr.transifex.com/
 
-[ ! x$1 == x ] && lang="-l $1" && shift 1
 args=$@
 
 modules="index contribute admin audit firststep rdp reboot update gui installagent installdocker installlinux installmedulla installvbox machine_group pulse_agent technicaloperations glpi deploy_agent imaging historique interface packages admin introduction audit computers"
+
 for mod in $modules
 do
-   cd $SCRIPT_PROJECT/../source/locale/
-   tx pull -a -f -r medullas-documentation.${mod} ${lang} ${args}
+    cd $SCRIPT_PROJECT/../source/locale/
+    tx push -r medullas-documentation.${mod} -s -t
+    #-f --no-interactive
 done
-
-
+~   
